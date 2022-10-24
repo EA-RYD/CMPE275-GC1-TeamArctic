7ea0176c7d9f414ad9852c738655c274b11b9b6d
Amy Phan <amyyyyphan99@gmail.com>
2022-10-23 17:01:20 -0700

    Edited server hook to use config file

--- src/gash/grpc/route/server/ConnectionHandler.java
+++ src/gash/grpc/route/server/ConnectionHandler.java
@@ -15,19 +15,20 @@ import com.google.protobuf.util.JsonFormat;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import route.Route;
 import route.RouteServiceGrpc;
 
 public class ConnectionHandler extends Thread {
 	private Socket connection;
+	private int destination;	// grpc leader server port
 	private InputStream in = null;
 	private PrintWriter out = null;
 	private BufferedReader reader = null;
 
-	public ConnectionHandler(Socket connection) {
+	public ConnectionHandler(Socket connection, int destination) {
 		this.connection = connection;
+		this.destination = destination;
 	}
 
 	@Override
@@ -35,12 +36,12 @@ public class ConnectionHandler extends Thread {
 		try {
 			in = connection.getInputStream();
 			out = new PrintWriter(connection.getOutputStream(), true);
-			reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			reader = new BufferedReader(new InputStreamReader(in));
 
 			if (in == null || out == null)
 				throw new RuntimeException("Unable to get in/out streams");
 
-			ManagedChannel ch = ManagedChannelBuilder.forAddress("localhost", 2345).usePlaintext().build();		// leader port
+			ManagedChannel ch = ManagedChannelBuilder.forAddress("localhost", destination).usePlaintext().build();
 			RouteServiceGrpc.RouteServiceStub stub = RouteServiceGrpc.newStub(ch);
 			final CountDownLatch finishLatch = new CountDownLatch(1);
 			StreamObserver<route.Route> responseObserver = new StreamObserver<route.Route>() {
@@ -53,7 +54,6 @@ public class ConnectionHandler extends Thread {
 
 				@Override
 				public void onError(Throwable t) {
-					Status status = Status.fromThrowable(t);
 					finishLatch.countDown();
 				}
 
@@ -63,37 +63,35 @@ public class ConnectionHandler extends Thread {
 				}
 			};
 		
-			while (true) {
-				String message;
-				if ((message = reader.readLine()) != null) {
-					// out.println(message);
-					System.out.println("Received: " + message);
-					System.out.flush();
+			String message;
+			while ((message = reader.readLine()) != null) {
+				System.out.println("Received: " + message);
+				System.out.flush();
 
-					// convert message to JSONObject and pass it back to ServerHook?
-					JSONObject json = new JSONObject(message);
-					Route.Builder bld = Route.newBuilder();
+				// convert message to JSONObject
+				JSONObject json = new JSONObject(message);
+				Route.Builder bld = Route.newBuilder();
 
-					// let Client know ServerHook received message
-					out.write("ServerHook received message: " + json.getLong("id"));
-					out.write('\n');
-					out.flush();
+				// let Client know ServerHook received message
+				out.write("ServerHook received message: " + json.getLong("id"));
+				out.write('\n');
+				out.flush();
 
-					// JsonFormat.parser().merge(message, bld);
+				// JsonFormat.parser().merge(message, bld);
 
-					// not ideal, need to figure out how to encode payload as byte in JSONObject and
-					// use JSONFormat.parser()
-					bld.setId(json.getLong("id"));
-					bld.setOrigin(json.getLong("origin"));
-					bld.setPath(json.getString("path"));
-					bld.setWorkType(json.getInt("workType"));
-					byte[] payload = json.getString("payload").getBytes();
-					bld.setPayload(ByteString.copyFrom(payload));
+				// not ideal, need to figure out how to encode payload as byte in JSONObject and
+				// use JSONFormat.parser()
+				bld.setId(json.getLong("id"));
+				bld.setOrigin(json.getLong("origin"));
+				bld.setPath(json.getString("path"));
+				bld.setWorkType(json.getInt("workType"));
+				byte[] payload = json.getString("payload").getBytes();
+				bld.setPayload(ByteString.copyFrom(payload));
 
-					// send to grpc server
-					stub.request(bld.build(), responseObserver);
-				}
+				// send to grpc server
+				stub.request(bld.build(), responseObserver);
 			}
+			connection.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
